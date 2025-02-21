247b9143c5ca5376733f592cea9521f3d410ac69
Dave <dave@mydigitalid.info>
2025-02-21 17:47:12 +1100

    update

--- ping-auth/network_handler.lua
+++ ping-auth/network_handler.lua
@@ -35,6 +35,7 @@ function _M.execute(config, parsed_url, payload)
         local client_cert, client_key = nil, nil
         if config.use_mtls then
             ngx.log(ngx.DEBUG, NAME .. " mTLS is enabled, retrieving client certificate.")
+            local access = require("kong.plugins.idp-ping-auth.access")
             client_cert, client_key = access.get_client_cert(config)
 
             if not client_cert or not client_key then
